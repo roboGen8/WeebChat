af8a2e14dc0e4aecbde42e26cfe65662a0b825aa
roboGen8 <genrocks88@gmail.com>
2021-02-11 15:42:52 -0500

    finish basic frontend

---
+++ components/InputBox/index.tsx
@@ -0,0 +1,56 @@
+import { AntDesign, Feather, FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+
+import styles from './style';
+
+const InputBox = () => {
+
+    const [message, setMessage] = useState('');
+
+    const onSendPress = () => {
+        console.log("send");
+        setMessage('');
+    }
+
+    const onSmilePress = () => {
+        console.log("smile");
+    }
+
+    const onPress = () => {
+        if (!message) {
+            onSmilePress();
+        } else {
+            onSendPress();
+        }
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.mainContainer}>
+                {/* <FontAwesome name="laugh-bream" size={24} color="grey" /> */}
+                <TextInput 
+                placeholder={"Type a message"}
+                style={styles.textInput} 
+                multiline
+                value={message}
+                onChangeText={setMessage}
+
+                />
+
+            </View>
+            <TouchableOpacity onPress={onPress}>
+            <View style={styles.buttonContainer}>
+                {!message
+                ? <AntDesign name="smileo" size={24} color="black" />
+                    : <Feather name="send" size={24} color="black" />   
+            }
+                
+            </View>
+            </TouchableOpacity>
+            
+        </View>
+    )
+}
+
+export default InputBox;
--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-import { useRoute } from '@react-navigation/native';
-import { FlatList } from 'react-native-gesture-handler';
-
-import chatRoomData from '../data/Chats';
 import ChatMessage from '../components/ChatMessage';
+import { FlatList } from 'react-native-gesture-handler';
+import InputBox from '../components/InputBox';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import chatRoomData from '../data/Chats';
+import { useRoute } from '@react-navigation/native';
 
 const ChatRoomScreen = () => {
     const route = useRoute();
 
     // console.log(route.params)
     return (
-        <FlatList
-            data={chatRoomData.messages}
-            renderItem={({item}) => <ChatMessage message={item}/>}
-            // inverted
-        ></FlatList>
+        <View>
+            <FlatList
+                data={chatRoomData.messages}
+                renderItem={({item}) => <ChatMessage message={item}/>}
+                // inverted
+            ></FlatList>
+            <InputBox />
+        </View>
+        
     )
 }
+
+// const styles = StyleSheet.create({
+//     container: {
+//       backgroundColor: 'grey',
+//     },
+//   });
 
 export default ChatRoomScreen;
